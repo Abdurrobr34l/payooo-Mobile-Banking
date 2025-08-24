ea2ab8ea8f949f22c9efd3d2f8daa742d58fe4e9
AbdurRob <abdurrobr34l@gmail.com>
2025-08-24 10:03:56 +0600

    Completed cashout js code

--- script/add-money.js
+++ script/add-money.js
@@ -1,3 +1,4 @@
+//* ADD MONEY
 const addAccountNumber = document.getElementById('add-money-account-number');
 const addAmount = document.getElementById('add-money-amount');
 const addPin = document.getElementById('add-money-pin');
@@ -8,7 +9,7 @@ addMoneyButton.addEventListener('click', function (reloadOff) {
 
   const availableMoney = document.getElementById('available-balance').innerText;
   const moneyHave = parseInt(availableMoney);
-  const bank = document.getElementById('add-money-bank').value;
+  // const bank = document.getElementById('add-money-bank').value;
   const accountNumber = parseInt(addAccountNumber.value);
   const amount = parseInt(addAmount.value);
   const pin = parseInt(addPin.value);
@@ -29,4 +30,38 @@ addMoneyButton.addEventListener('click', function (reloadOff) {
   addAccountNumber.value = "";
   addPin.value = "";
   addAmount.value = "";
+})
+
+//* CASH OUT
+const cashOutAccountNumber = document.getElementById('cashout-account-number');
+const cashOutAmount = document.getElementById('cashout-amount');
+const cashOutPin = document.getElementById('cashout-pin');
+const cashOutButton = document.getElementById('cashout-button');
+
+cashOutButton.addEventListener('click', function (reloadOff) {
+  reloadOff.preventDefault();
+
+  const availableMoney = document.getElementById('available-balance').innerText;
+  const moneyHave = parseInt(availableMoney);
+  // const bank = document.getElementById('add-money-bank').value;
+  const accountNumber = parseInt(cashOutAccountNumber.value);
+  const amount = parseInt(cashOutAmount.value);
+  const pin = parseInt(cashOutPin.value);
+
+  if (cashOutAccountNumber.value.length != 11) {
+    alert("Please provide valid account number")
+    return;
+  }
+
+  if (pin != 1234) {
+    alert("Pin is invalid");
+    return;
+  }
+
+  let newBalance = moneyHave - amount;
+  document.getElementById('available-balance').innerText = newBalance;
+
+  cashOutAccountNumber.value = "";
+  cashOutAmount.value = "";
+  cashOutPin.value = "";
 })
